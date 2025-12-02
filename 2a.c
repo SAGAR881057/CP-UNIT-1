e11f2656a5ee8d95666d9a5057bf47ac68bd14e5
SAGAR881057 <kingsagar8829@gmail.com>
2025-12-02 11:35:15 +0530

    Add files via upload

---
+++ 2a.c
@@ -0,0 +1,22 @@
+// P2a.c - C program to display different data types
+#include <stdio.h>
+
+int main() {
+    // Integer type
+    int a = 10;
+    
+    // Floating-point types
+    float b = 3.14f;
+    double c = 9.87654321;
+    
+    // Character type
+    char d = 'Z';
+    
+    // Output the values and their types
+    printf("Integer (int): %d\n", a);
+    printf("Float (float): %f\n", b);
+    printf("Double (double): %lf\n", c);
+    printf("Character (char): %c\n", d);
+    
+    return 0;
+}
