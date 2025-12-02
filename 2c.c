952db4f687253243bb7d78d7e6dce1e74dfea1f9
SAGAR881057 <kingsagar8829@gmail.com>
2025-12-02 11:37:49 +0530

    Add files via upload

---
+++ 2c.c
@@ -0,0 +1,16 @@
+// P2c.c - C Program to Print Size of Each Data Type
+#include <stdio.h>
+
+int main() {
+    // Use sizeof() operator to find the size in bytes
+    printf("Size of 'char': %zu byte(s)\n", sizeof(char));
+    printf("Size of 'int': %zu byte(s)\n", sizeof(int));
+    printf("Size of 'short int': %zu byte(s)\n", sizeof(short int));
+    printf("Size of 'long int': %zu byte(s)\n", sizeof(long int));
+    printf("Size of 'long long int': %zu byte(s)\n", sizeof(long long int));
+    printf("Size of 'float': %zu byte(s)\n", sizeof(float));
+    printf("Size of 'double': %zu byte(s)\n", sizeof(double));
+    printf("Size of 'long double': %zu byte(s)\n", sizeof(long double));
+    
+    return 0;
+}
